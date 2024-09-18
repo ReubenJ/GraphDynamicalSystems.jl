7b9799abbbf84389c8ab4b100c682551a2a16c17
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2024-09-18 18:42:01 +0200

    Reconstruct networks using CNF grammar

--- experiments/SynthBN/scripts/synthesize_bn.jl
+++ experiments/SynthBN/scripts/synthesize_bn.jl
@@ -10,6 +10,7 @@ all_params = Dict(
     "network_size" => 10,
     "max_equation_depth" => 3,
     "iterations" => 1000,
+    "repetitions" => 10,
     "seed" => collect(0:9),
 )
 dicts = dict_list(all_params)
@@ -22,7 +23,76 @@ for (i, d) in enumerate(dicts)
     per_iter_data, file =
         @produce_or_load(get_split_state_space, d, datadir("sims", "specs"))
     data[i] = per_iter_data
-    @show file
 end
 
-@show data
+res = collect_results!(datadir("sims", "specs"))
+
+using Herb, HerbGrammar, SoleLogics, HerbSpecification, HerbSearch
+
+function evaluate(problem, expr, symboltable)
+    sat_examples = 0
+
+    for example ∈ problem.spec
+        res = interpret(eval(expr), TruthDict(Dict(enumerate(example.in[:state]))))
+        sat_examples += res.flag == example.out
+    end
+
+    return sat_examples / length(problem.spec)
+end
+
+cnf_grammar = @cfgrammar begin
+    CNF = Disj ∧ CNF
+    CNF = Disj
+    Disj = Lit ∨ Disj
+    Disj = Lit
+    Lit = Var
+    Lit = ¬Var
+end
+
+for i = 1:all_params["network_size"]
+    add_rule!(cnf_grammar, :(Var = Atom($i)))
+end
+
+break_after = 10
+
+for experiment in eachrow(res)
+    println("Experiment: ", experiment.seed)
+
+    for (node, examples) in experiment.specifications
+        println("Node: ", node)
+
+        io_examples =
+            map(((in, out),) -> IOExample(Dict([:state => in]), out), collect(examples))
+        problem = Problem("$node", io_examples)
+
+        iterator = BFSIterator(cnf_grammar, :CNF, max_depth = 5)
+        found = []
+        all_ex = []
+        found_after = Inf
+        for (i, ex) in enumerate(iterator)
+            expr = rulenode2expr(ex, cnf_grammar)
+
+            symboltable = SymbolTable(cnf_grammar, Main)
+            score = evaluate(problem, expr, symboltable)
+
+            push!(all_ex, (eval(expr), score))
+
+            if score == 1
+                push!(found, eval(expr))
+            end
+            if length(found) > break_after
+                found_after = i
+                break
+            end
+        end
+        d = Dict([
+            "seed" => experiment.seed,
+            "node" => node,
+            "found" => found,
+            "all_ex" => all_ex,
+            "break_after" => break_after,
+            "found_after" => found_after,
+        ])
+        @tagsave(datadir("exp_raw", "cnf_search", savename(d, "cnf_search.jld2")), d)
+    end
+end
--- experiments/SynthBN/src/gather_bn_data.jl
+++ experiments/SynthBN/src/gather_bn_data.jl
@@ -7,13 +7,13 @@ end
 
 function split_state_space(trajectory::StateSpaceSet)
     # split into pairs of input (all values) and output (changed value)
-    input_output_pairs_per_node = Dict{Int,Vector{Tuple{Vector{Int},Int}}}()
+    input_output_pairs_per_node = Dict{Int,Set{Tuple{Vector{Int},Int}}}()
     for i = 1:length(trajectory)-1
         changed = findfirst(trajectory[i+1] .!= trajectory[i])
         # only proceed if there was a change
         if !isnothing(changed)
             new_value = trajectory[i+1][changed]
-            existing_pairs = get(input_output_pairs_per_node, changed, [])
+            existing_pairs = get(input_output_pairs_per_node, changed, Set())
             push!(existing_pairs, (trajectory[i], new_value))
             input_output_pairs_per_node[changed] = existing_pairs
         end
@@ -35,18 +35,31 @@ function get_split_state_space(
 end
 
 function get_split_state_space(params::Dict{String,<:Any})
-    @unpack network_size, max_equation_depth, iterations, seed = params
+    @unpack network_size, max_equation_depth, iterations, repetitions, seed = params
 
     fulld::Dict{String,Any} = copy(params)
 
-    bn = sample_boolean_network(network_size, max_equation_depth, seed)
-    async_bn = abn(bn; seed = seed)
-    trajectory = gather_bn_data(async_bn, iterations)
-    divided_state_space = split_state_space(trajectory)
+    all_divided_state_space = Dict{Int,Set{Tuple{Vector{Int},Int}}}()
+    all_abns = Dict{Int,Any}()
 
-    fulld["specifications"] = divided_state_space
+    bn = sample_boolean_network(network_size, max_equation_depth, seed)
+    for r = 1:repetitions
+        async_bn = abn(bn; seed = seed * r)
+        trajectory = gather_bn_data(async_bn, iterations)
+        divided_state_space = split_state_space(trajectory)
+        for (node, pairs) in divided_state_space
+            existing_pairs = get(all_divided_state_space, node, Set())
+            push!(existing_pairs, pairs...)
+            all_divided_state_space[node] = existing_pairs
+        end
+        all_abns[r] = async_bn
+    end
+    # trajectory = gather_bn_data(async_bn, iterations)
+    # divided_state_space = split_state_space(trajectory)
+
+    fulld["specifications"] = all_divided_state_space
     fulld["bn"] = bn
-    fulld["async_bn"] = async_bn
+    fulld["abns"] = all_abns
 
     return fulld
 end
