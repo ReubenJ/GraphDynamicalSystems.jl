f04eb8c02bb55194f8a9b30b80972847eac5aa89
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2024-10-31 12:52:21 +0100

    Reduce cpus for synth

--- experiments/SynthBN/scripts/synth_biodivine.sh
+++ experiments/SynthBN/scripts/synth_biodivine.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name="Synthesize Biodivine"
 #SBATCH --partition=compute
 #SBATCH --time=02:00:00
-#SBATCH --cpus-per-task=64
+#SBATCH --cpus-per-task=48
 #SBATCH --mem-per-cpu=1G
 #SBATCH --account=innovation
 
 module load 2024r1 julia
 
-srun julia -t 64 --project=. scripts/synth_biodivine.jl
+srun julia -t 48 --project=. scripts/synth_biodivine.jl
