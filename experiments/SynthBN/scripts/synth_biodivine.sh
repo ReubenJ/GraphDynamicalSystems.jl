9a2c40e731db408b6445a93aab46056e45d8a3fb
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2024-10-31 12:46:51 +0100

    Reduce memory for synth

--- experiments/SynthBN/scripts/synth_biodivine.sh
+++ experiments/SynthBN/scripts/synth_biodivine.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=compute
 #SBATCH --time=02:00:00
 #SBATCH --cpus-per-task=64
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=1G
 #SBATCH --account=innovation
 
 module load 2024r1 julia
