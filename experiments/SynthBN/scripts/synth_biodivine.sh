5f81405a49716cccf2f0ffce3ec2c27cbf612da5
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2024-10-31 13:06:22 +0100

    Increase memory

--- experiments/SynthBN/scripts/synth_biodivine.sh
+++ experiments/SynthBN/scripts/synth_biodivine.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=compute
 #SBATCH --time=02:00:00
 #SBATCH --cpus-per-task=16
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=3G
 #SBATCH --account=innovation
 
 module load 2024r1 julia
