ba7ac6aefbddf594b91e15be2a3d6dd4d32ce745
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2024-11-01 10:37:00 +0100

    Add custom interpreter

--- experiments/SynthBN/src/evaluator.jl
+++ experiments/SynthBN/src/evaluator.jl
@@ -1,9 +1,40 @@
+using MLStyle: @match
+using SoleLogics
+import SoleLogics.interpret
+
+function interpret(φ::Expr, i::SoleLogics.AbstractInterpretation, args...; kwargs...)
+    syntax_branch = @match φ begin
+        Expr(:call, :Atom, val) => interpret(Atom(val), i, args...; kwargs...)
+        Expr(:call, :∨, children...) => interpret(
+            SyntaxBranch(∨, [interpret(ch, i, args...; kwargs...) for ch in children]...),
+            i,
+            args...;
+            kwargs...,
+        )
+        Expr(:call, :∧, children...) => interpret(
+            SyntaxBranch(∧, [interpret(ch, i, args...; kwargs...) for ch in children]...),
+            i,
+            args...;
+            kwargs...,
+        )
+        Expr(:call, :¬, child) => interpret(
+            SyntaxBranch(¬, interpret(child, i, args...; kwargs...)),
+            i,
+            args...;
+            kwargs...,
+        )
+        _ => error("missing a match for $φ")
+    end
+
+    return interpret(syntax_branch, i, args...; kwargs...)
+end
+
 function evaluate_bn(problem, expr)
     sat_examples = 0
 
     for example ∈ problem.spec
         truth = TruthDict(Dict(enumerate(example.in[:state])))
-        res = interpret(eval(expr), truth)
+        res = interpret(expr, truth)
         sat_examples += res.flag == example.out
     end
 
