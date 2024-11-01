01713d81e7bf3870b247a72927a747b338f595a5
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2024-11-01 12:03:49 +0100

    Increase cores

--- experiments/SynthBN/scripts/synth_biodivine.sh
+++ experiments/SynthBN/scripts/synth_biodivine.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name="Synthesize Biodivine"
 #SBATCH --partition=compute
 #SBATCH --time=24:00:00
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=32
 #SBATCH --mem-per-cpu=3G
 #SBATCH --account=innovation
 
 module load 2024r1 julia
 
-srun julia -t 16 --project=. scripts/synth_biodivine.jl
+srun julia -t 32 --project=. scripts/synth_biodivine.jl
