1bb2f6395958d466a9ab85ab9a69323ab803b841
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2025-01-15 10:26:03 -0600

    Split script use distributed

--- experiments/Synth/scripts/split_biodivine_trajectories.jl
+++ experiments/Synth/scripts/split_biodivine_trajectories.jl
@@ -1,33 +1,46 @@
-"""
-Takes the biodivine trajectories and divides them into i/o pairs for synthesis
-"""
+#!/usr/bin/env -S julia --project=.
+#
+#SBATCH --job-name="Split"
+#SBATCH --partition=compute
+#SBATCH --time=02:00:00
+#SBATCH --ntasks 64
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --account=research-eemcs-st
 
-using DrWatson
+using Distributed
+try
+    using SlurmClusterManager
+    addprocs(SlurmManager())
+catch
+    @info "Not running from within Slurm, proceeding without Slurm workers"
+end
 
-@quickactivate :Synth
+@everywhere using DrWatson
+
+@everywhere quickactivate(pwd())
+@everywhere using Synth
 
 using JLD2
+using ProgressMeter
 
 trajectories_dir = datadir("sims", "biodivine_trajectories")
 split_trajectories_dir = datadir("sims", "biodivine_split")
 
-for traj_file in readdir(datadir("sims", "biodivine_trajectories"))
-    traj_path = joinpath(trajectories_dir, traj_file)
-    split_path = joinpath(split_trajectories_dir, traj_file)
-
-    if !isfile(split_path)
-        @info "Splitting $traj_file..."
-        traj_file_contents = load(traj_path)
+@showprogress pmap(readdir(datadir("sims", "biodivine_trajectories"))) do traj_file
+    @produce_or_load(
+        traj_file,
+        path = split_trajectories_dir,
+        filename = traj_file
+    ) do traj_file
+        traj_file_contents = load(joinpath(trajectories_dir, traj_file))
         if "trajectories" in keys(traj_file_contents)
             trajectories = traj_file_contents["trajectories"] # also includes git tag, etc.
 
             split_traj = split_state_space.(trajectories)
-            @tagsave(split_path, @strdict split_traj)
+            return @strdict split_traj
         else
             @info "No trajectory found for $traj_file"
         end
-
-    else
-        @info "$split_path already exists"
     end
 end
