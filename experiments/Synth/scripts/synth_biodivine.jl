bc8413b0b4efdaf5ba920da66984a931bf4e7e6d
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2025-01-28 20:02:51 +0100

    Re-up memory

--- experiments/Synth/scripts/synth_biodivine.jl
+++ experiments/Synth/scripts/synth_biodivine.jl
@@ -2,10 +2,10 @@
 #
 #SBATCH --job-name="Synth"
 #SBATCH --partition=compute
-#SBATCH --time=00:30:00
-#SBATCH --ntasks 16
+#SBATCH --time=02:00:00
+#SBATCH --ntasks 256
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=2G
+#SBATCH --mem-per-cpu=8G
 #SBATCH --account=research-eemcs-st
 
 using Distributed
