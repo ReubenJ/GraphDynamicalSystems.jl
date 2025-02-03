e97e6e3323c5bfd77c676d2272a7fc7dd1839a46
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2025-02-03 16:28:59 +0100

    Uncomment tests

--- experiments/Synth/test/runtests.jl
+++ experiments/Synth/test/runtests.jl
@@ -13,13 +13,13 @@ ti = time()
 testdir = (x...) -> projectdir("test", x...)
 
 @testset "Synth tests" begin
-    # @testset "Sample Trajectories" begin
-    #     @unpack model = load(joinpath(@__DIR__, "ex_model.jld2"))
-    #     async_bn = BooleanNetworks.abn(model.graph)
-    #     @test !isnothing(gather_bn_data(async_bn, 1))
-    # end
+    @testset "Sample Trajectories" begin
+        @unpack model = load(joinpath(@__DIR__, "ex_model.jld2"))
+        async_bn = BooleanNetworks.abn(model.graph)
+        @test !isnothing(gather_bn_data(async_bn, 1))
+    end
 
-    # include(testdir("test_neighbor_counting.jl"))
+    include(testdir("test_neighbor_counting.jl"))
     include(testdir("test_biodivine_pipeline.jl"))
 end
 
