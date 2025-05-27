439f2b25004400437e5b0bdb93db4e8ddd97e10f
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2025-05-27 12:51:38 +0200

    Remove synthesis experiments

--- experiments/Synth/src/create_problem.jl
+++
@@ -1,9 +0,0 @@
-function _e2p(n, e)
-    _pair_to_undirected(p) = UndirectedExample(Dict(:state => p[1]), Dict(:state => p[2]))
-
-    return UndirectedProblem(n, _pair_to_undirected.(e))
-end
-
-examples_to_problem(node::Integer, examples::AbstractSet) = _e2p(string(node), examples)
-examples_to_problem(node::Atom, examples) = _e2p(node.value, examples)
-examples_to_problem(node::AbstractString, examples) = _e2p(node, examples)
