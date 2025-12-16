227cc154c22f16409965ea331a34c0e0d151460d
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2025-12-16 17:31:18 +0100

    Make `GDS <: DiscreteDynamicalSystem`

--- src/GraphDynamicalSystems.jl
+++ src/GraphDynamicalSystems.jl
@@ -3,32 +3,9 @@ module GraphDynamicalSystems
 using DocStringExtensions
 
 include("gds_interface.jl")
-export GraphDynamicalSystem,
-    GDS,
-    ScheduleStyle,
-    Asynchronous,
-    Synchronous,
-    get_n_entities,
-    entities,
-    get_schedule,
-    get_state,
-    get_graph
 
 include("qualitative_networks.jl")
-export QualitativeNetwork,
-    QN,
-    Entity,
-    build_qn_grammar,
-    update_functions_to_interaction_graph,
-    sample_qualitative_network,
-    get_domain,
-    target_functions,
-    interpret,
-    create_qn_system,
-    default_target_function,
-    set_state!,
-    current_parameters
 
-include("io/bma.jl")
+# include("io/bma.jl")
 
 end
