b3161fd4945aaae42ea4901e8c83ebe7b609a890
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2026-03-16 21:53:30 +0100

    build: add flake for dev. env

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            julia-bin
+            prek
+          ];
+        };
+      }
+    );
+}
