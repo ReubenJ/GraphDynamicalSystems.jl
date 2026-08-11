61cd64618048fa7a6e36d7ada8b2a86292e0631a
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2026-08-11 19:50:21 +0200

    feat: add `Determinism` trait to schedules
    Fix a bug with `Asynchronous` schedules

--- src/schedule.jl
+++ src/schedule.jl
@@ -1,6 +1,6 @@
 module Schedule
 using Compat: @compat
-using ..Interface: vertices
+using ..Interface: vertices, schedule_style
 
 @compat public ScheduleStyle,
     Asynchronous,
@@ -16,9 +16,44 @@ Two basic common schedule types are [`Asynchronous`](@ref) and
 [`Synchronous`](@ref).
 
 A new schedule type `MySchedule<:ScheduleStyle` should implement
-`to_update(::MySchedule, gds)`.
+`to_update(::MySchedule, gds)` and `determinism(::MySchedule)`.
 """
 abstract type ScheduleStyle end
+
+"""
+    Determinism
+
+Trait defining the determinism of a schedule style.
+"""
+abstract type Determinism end
+struct Deterministic <: Determinism end
+struct NonDeterministic <: Determinism end
+
+"""
+    is_deterministic(gds)
+
+Whether the `gds` has a deterministic schedule.
+"""
+function is_deterministic(gds)
+    return is_deterministic(schedule_style(gds))
+end
+
+"""
+    is_deterministic(s::ScheduleStyle)
+
+Whether schedule `s` is deterministic.
+"""
+function is_deterministic(s::ScheduleStyle)
+    return is_deterministic(determinism(s))
+end
+
+function is_deterministic(::Deterministic)
+    return true
+end
+
+function is_deterministic(::NonDeterministic)
+    return false
+end
 
 """
     to_update(gds)
@@ -39,7 +74,11 @@ An asynchronous schedule randomly chooses a vertex to update at each timestep.
 struct Asynchronous <: ScheduleStyle end
 
 function to_update(::Asynchronous, gds)
-    return rand(vertices(gds), 1)
+    return rand(collect(vertices(gds)), 1)
+end
+
+function determinism(::Asynchronous)
+    return NonDeterministic()
 end
 
 """
@@ -52,4 +91,9 @@ struct Synchronous <: ScheduleStyle end
 function to_update(::Synchronous, gds)
     return vertices(gds)
 end
+
+function determinism(::Synchronous)
+    return Deterministic()
+end
+
 end
