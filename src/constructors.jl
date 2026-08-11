d145c57013b291c209eefe8e94df930741c73c47
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2026-08-11 19:48:40 +0200

    feat: add `Constructors` module for generic constructor methods

---
+++ src/constructors.jl
@@ -0,0 +1,22 @@
+module Constructors
+using Compat: @compat
+
+@compat public vertex_function_to_edgelist
+
+"""
+    vertex_function_to_edgelist(vertex_fn_pairs)
+    vertex_function_to_edgelist(vertex, fn, all_vertices)
+
+Create a list of edges based on the vertices that appear in `fn`.
+
+If a vertex `X` has a function `X + Y` and `all_vertices = [X, Y, ...]`,
+then return edges `X => X` and `X => Y`.
+
+Requires `AbstractTrees` to be loaded.
+"""
+function vertex_function_to_edgelist(vertex_fn_pairs)
+    return vcat(vertex_function_to_edgelist.(first.(vertex_fn_pairs), last.(vertex_fn_pairs), (first.(vertex_fn_pairs),))...)
+end
+# Second signature implemented in `AbstractTreesExt`
+
+end
